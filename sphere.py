4396eb959cfe2cdb0171674e1876a922ec77d885
yusuf239 <77644797+yusuf239@users.noreply.github.com>
2022-04-12 21:24:58 -0500

    Update sphere.py

--- sphere.py
+++ sphere.py
@@ -1,10 +1,13 @@
 # inspiration code for Python Unit Testing Project
 
-def surfaceArea():
-    pass
+import math
 
-def volume():
-    pass
+def surfaceArea(rad):
+    surfaceArea = 4 * math.pi * rad * rad
+    return surfaceArea
+
+def volume(rad):
+    volume = 4/3 * math.pi * rad * rad * rad
 
 def prompt():
     print()
@@ -12,6 +15,9 @@ def prompt():
     print("PYTHON PROGRAM TO FIND THE VOLUME AND SURFACE AREA OF SPHERE")
     print("------------------------------------------------------------")
     radius = int(input("Please Enter the radius :"))
+    
+    print("\nThe Volume of a Sphere = ", volume(radius))
+    print("\nThe Surface Area of a Sphere = ", surfaceArea(radius))
 
 if __name__ == '__main__':
     prompt()
